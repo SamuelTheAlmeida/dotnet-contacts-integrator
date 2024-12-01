bedb233f1f2c29872b1d2bc85a63e36723577170
samuel.almeida <samuel.a.almeida@capgemini.com>
2024-12-01 20:48:56 -0300

    added commented out service test, client classes

--- ContactsIntegrator/ContactsIntegrator.SDK.MailChimp/MailchimpClient.cs
+++ ContactsIntegrator/ContactsIntegrator.SDK.MailChimp/MailchimpClient.cs
@@ -4,5 +4,11 @@ namespace ContactsIntegrator.SDK.MailChimp
 {
     public class MailchimpClient : IMailchimpClient
     {
+        public MailchimpClient() { }
+
+        public async Task AddListAsync()
+        {
+            var restClient = new RestClient("https://api.mailchimp.com/3.0/lists");
+        }
     }
 }
